bc0423cc0644094dea0a5d3c76d79b054911f0e7
Evan Black <evan@erblack.com>
2017-06-05 08:54:14 -0500

    Prevent error from bubbling up

--- jobs/message_sender.rb
+++ jobs/message_sender.rb
@@ -8,8 +8,12 @@ class MessageSender
   def self.perform(message_id)
     message = Message.find(id: message_id)
     options = { from: message.from, to: message.to, text: message.text }
-    result = Bandwidth::Message.create(BANDWIDTH_CLIENT, options)
-    message.update(external_id: result[:message_id], state: result[:state])
+    begin
+      result = Bandwidth::Message.create(BANDWIDTH_CLIENT, options)
+      message.update(external_id: result[:message_id], state: result[:state])
+    rescue
+      nil
+    end
   end
 end
 
